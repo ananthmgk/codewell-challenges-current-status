95933b51d3913fcc8335f8c239387da7bdff0c22
Ramo <ramo.phoenix7@gmail.com>
2021-10-22 21:34:14 +0530

    Updated table css and code enhancements performed in main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+const headers = ["Projects", "Status", "Source Code", "Live Demo"]
 const projectsData = [
   {
     "id": 1,
     "title": "Gradie Sign Up",
     "url": "https://www.codewell.cc/challenges/gradie-sign-up-page--608ac420650dff001599e8ec",
-    "status":`<td class="yellow"> completed </td>` ,
+    "status": "completed",
     "source": "https://github.com/ananthmgk/Gradie-Signup-Page",
     "demo": "https://ananthmgk.github.io/Gradie-Signup-Page/"
   },
@@ -11,7 +12,7 @@ const projectsData = [
     "id": 2,
     "title": "snipper landing page",
     "url": "https://www.codewell.cc/challenges/snipper-landing-page--608bbe67e0984a001540d79b",
-    "status": `<td class="yellow"> completed </td>` ,
+    "status": "completed",
     "source": "https://github.com/ananthmgk/Snippet_Template_Starter_Files",
     "demo": "https://ananthmgk.github.io/Snippet_Template_Starter_Files/"
   },
@@ -19,7 +20,7 @@ const projectsData = [
     "id": 3,
     "title": "Commune Waitlist",
     "url": "https://www.codewell.cc/challenges/commune-waitlist-page--608d9565747bad001532bd64",
-    "status": `<td class="yellow"> completed </td>` ,
+    "status": "completed",
     "source": "https://github.com/ananthmgk/Commune-Waitlist-codewell-proj",
     "demo": "https://ananthmgk.github.io/Commune-Waitlist-codewell-proj/"
   },
@@ -27,113 +28,110 @@ const projectsData = [
     "id": 4,
     "title": "Spense landing page",
     "url": "https://www.codewell.cc/challenges/spense-landing-page--608a7a859691700015db16c5",
-    "status": `<td class="inprogress-color">in Progress</td>`
+    "status": "in_progress"
   },
   {
     "id": 5,
     "title": "Chirp landing page",
     "url": "https://www.codewell.cc/challenges/chirp-landing-page--60fc1e36a383e41090a3c71c",
-    "status": `<td class="red">TO DO</td>`
+    "status": "todo"
   },
   {
     "id": 6,
     "title": "Comment module",
     "url": "https://www.codewell.cc/challenges/comment-module--608bc18ee0984a001540d7a6",
-    "status": `<td class="red">TO DO</td>`
+    "status": "todo"
   },
   {
     "id": 7,
     "title": "Fiber landing page",
     "url": "https://www.codewell.cc/challenges/fiber-landing-page--608a7e639691700015db16d1",
-    "status": `<td class="red">TO DO</td>`
+    "status": "todo"
   },
   {
     "id": 8,
     "title": "hosterr waitlist page",
     "url": "https://www.codewell.cc/challenges/hosterr-waitlist-page--60b3ea4c0cc72310b5a2494d",
-    "status": `<td class="red">TO DO</td>`
+    "status": "todo"
   },
   {
     "id": 9,
     "title": "indie brew landing page",
     "url": "https://www.codewell.cc/challenges/indiebrew-landing-page--608aca51650dff001599e8f9",
-    "status": `<td class="red">TO DO</td>`
+    "status": "todo"
   },
   {
     "id": 10,
     "title": "meet waitlist template",
     "url": "https://www.codewell.cc/challenges/meeet-waitlist-template--60e05defa383e41090a3c273",
-    "status": `<td class="red">TO DO</td>`
+    "status": "todo"
   },
   {
     "id": 11,
     "title": "news letter popup",
     "url": "https://www.codewell.cc/challenges/newsletter-popup-template--60a8db35203a0e6180bb5035",
-    "status": `<td class="red">TO DO</td>`
+    "status": "todo"
   },
   {
     "id": 12,
     "title": "time now landing page",
     "url": "https://www.codewell.cc/challenges/timenow-landing-page--608d9a7d747bad001532bd70",
-    "status": `<td class="red">TO DO</td>`
+    "status": "todo"
   },
   {
     "id": 13,
     "title": "Unifeed Blog page",
     "url": "https://www.codewell.cc/challenges/unifeed-blog-page--608d9d5c747bad001532bd7c",
-    "status": `<td class="red">TO DO</td>`
+    "status": "todo"
   },
   {
     "id": 14,
     "title": "Yelp Camp",
     "url": "https://www.codewell.cc/challenges/yelpcamp-by-colt-steele--6144c7c8a383e41090a3d84b",
-    "status": `<td class="red">TO DO</td>`
+    "status": "todo"
   },
 ]
 
-function getRow(rowData) {
-  let rowText = []
-  rowText.push('<tr>')
-  rowText.push(`<td class="pink"> ${rowData.id}. </td>`)
-  rowText.push(`<td class="project-color"> <a class="hover" href="${rowData.url}" target="_blank"> ${rowData.title} </a></td>`)
-  rowText.push(` ${rowData.status} `)
-  rowText.push(`<td class="source-color"> ${getAnchorTag(rowData.source)}</td>`)
-  rowText.push(`<td class="source-color"> ${getAnchorTag(rowData.demo)}</td>`)
-  rowText.push('</tr>')
-  return rowText.join("\n")
+const statusMap = {
+  'completed': 'Completed',
+  'todo': 'Todo',
+  'in_progress': 'In Progress'
 }
 
-function getAnchorTag(url) {
+function getRow(rowData) {
+  let rowText = []
+  rowText.push(`<td> ${getAnchorTag(rowData.url, rowData.title)}</td>`)
+  rowText.push(`<td> ${statusMap[rowData.status]} </td>`)
+  rowText.push(`<td> ${getAnchorTag(rowData.source)}</td>`)
+  rowText.push(`<td> ${getAnchorTag(rowData.demo)}</td>`)
+  return `<tr> ${rowText.join("\n")} </tr>`
+}
+
+function getAnchorTag(url, text='Click here to view') {
   if (url) {
-    return `<a class="hover" href="${url}" target="_blank"> Click here to view </a>`
+    return `<a href="${url}" target="_blank"> ${text} </a>`
   } else {
     return "N/A"
   }
 }
 
-function getHeader(headerData) {
-  let rowText = []
-  rowText.push('<tr>')
-  rowText.push(`<th class="pink"> ${headerData[0]} </th>`)
-  rowText.push(`<th class="project-color"> ${headerData[1]} </th>`)
-  rowText.push(`<th class="yellow"> ${headerData[2]} </th>`)
-  rowText.push(`<th class="source-color"> ${headerData[3]} </th>`)
-  rowText.push(`<th class="live-color"> ${headerData[4]} </th>`)
-  rowText.push('</tr>')
-  return rowText.join("\n")
+function getHeader() {
+  let headerRows = [];
+  for (const header of headers) {
+    headerRows.push(`<th> ${header} </th>`);
+  }
+  return `<tr> ${headerRows.join("\n")} </tr>`;
 }
 
 function getTable() {
   let tableData = []
-  let headers = ["S.No", "Projects", "Status", "Source Code", "Live Demo"]
-  tableData.push(getHeader(headers))
+  tableData.push(getHeader())
   for (const project of projectsData) {
     tableData.push(getRow(project))
   }
   return tableData.join("\n");
 }
 
-function setTableData() {
-  console.log("setTableData() called")
-  document.getElementById('table').innerHTML = getTable()
-}
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById('table').innerHTML = getTable();
+});
